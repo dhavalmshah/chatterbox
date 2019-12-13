f6c8ce7de8f7f6d9cfb3a5f5a3034f4f28b424fb
Dhaval Shah <dhavalmshah@gmail.com>
2019-12-13 10:04:00 +0530

    first commit

---
+++ src/test/java/in/smoothline/chatterbox/ChatterboxApplicationTests.java
@@ -0,0 +1,13 @@
+package in.smoothline.chatterbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
