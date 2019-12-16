5f460437b5f7be2c72e2f6e855b4b7ff6b87e259
Surag19 <surag.doshi@gmail.com>
2019-12-16 17:13:47 +0000

    Created User Profile Controller

---
+++ src/main/java/in/smoothline/chatterbox/vo/UserProfile.java
@@ -0,0 +1,26 @@
+package in.smoothline.chatterbox.vo;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@ToString
+public class UserProfile
+{
+    
+    private String user_id;
+    private String password;
+    private String creation_ts;
+    private String last_modified_ts;
+    private String is_admin;
+    private String deactivated;
+    private String email;
+    private String mobile;
+}
